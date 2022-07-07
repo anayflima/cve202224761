bf42cc20539bdd59e27588f3a92fcef78dc5d854
anayflima <ayf.lima@usp.br>
2022-07-07 14:32:50 -0300

    Add test with signed content length

--- test_signed_content_lengths.sh
+++ test_signed_content_lengths.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-(printf 'GET http://localhost:6789/testeget HTTP/1.1\r\n'\
-'Transfer-Encoding: chunked\r\n'\
-'Host: localhost\r\n\r\n'\
-"2;\nxx\r\n"\
-'47\r\n'\
-'0\r\n'\
+# The frontend sees two requests to /firstGet,
+# while Waitress sees a request to /firstGet
+# and another one to /secondGet.
+
+(printf 'GET http://localhost:6789/firstGet HTTP/1.1\r\n'\
+'Content-Length: +53\r\n'\
 '\r\n'\
-'GET http://localhost:6789/testeget HTTP/1.1\r\n'\
-'Transfer-Encoding: chunked\r\n'\
-'Host: localhost\r\n\r\n'\
-'0\r\n'\
+'GET http://localhost:6789/firstGet HTTP/1.1\r\n'\
+'Dummy: GET http://localhost:6789/secondGet HTTP/1.1\r\n'\
 '\r\n'; sleep 1) | nc localhost 6789
